92283f08ca54d52538a5362cd349f132678765b9
René Reimann <rene.reimann@rwth-aachen.de>
2020-02-07 08:30:46 +0100

    made digitized TS parametrization into a txt file and handle it properly in test_data

--- external_data/__init__.py
+++ external_data/__init__.py
@@ -1,15 +1,19 @@
+import numpy as np
+
 from ps_analysis.util.digitizer import digitizer
+
 from .Check_Poisson_Distribution_Stefans_diss import N_spots_hist_threshold as _N_spots_hist_threshold
 from .Expectation_Stefans_diss_Fig_7_17 import expectation_vs_threshold_digitized as _expectation_vs_threshold_digitized
 from .Sensitivity_Disc_pot_UL_PS_paper_Fig_10_arxiv_1609_04981 import sensitivity_digitized as _sensitivity_digitized
 from .Sensitivity_Disc_pot_UL_PS_paper_Fig_10_arxiv_1609_04981 import discovery_potential_digitized as _discovery_potential_digitized 
 from .Sensitivity_Disc_pot_UL_PS_paper_Fig_10_arxiv_1609_04981 import upper_limit_90_per_cent_digitized as _upper_limit_90_per_cent_digitized
 from .Sensitivity_Disc_pot_UL_PS_paper_Fig_10_arxiv_1609_04981 import diffuse_flux_example_digitized as _diffuse_flux_example_digitized
-from .TS_parametrization import TS_parametrization_digitized_NDoF as _TS_parametrization_digitized_NDoF
-from .TS_parametrization import TS_parametrization_digitized_eta as _TS_parametrization_digitized_eta
-from .Sensitivity_E_3 import discovery_potential_E_3_digitized as _discovery_potential_E_3_digitized
-from .Sensitivity_E_3 import sensitivity_E_3_digitized as _sensitivity_E_3_digitized
-from .Sensitivity_E_2 import limit_E_2_digitized as _limit_E_2_digitized
+_TS_parametrization_digitized_eta = np.genfromtxt("TS_parametrization_NDoF_Stefan.txt", delimiter=", ", comments="#", names=["sinDec", "eta"])
+_TS_parametrization_digitized_NDoF = np.genfromtxt("TS_parametrization_NDoF_Stefan.txt", delimiter=", ", comments="#", names=["sinDec", "NDoF"])
+_limit_E_2_digitized = np.genfromtxt("Sensitivity_E_2_7yr_PS_paper.txt", delimiter=", ", comments="#", names=["sinDec", "flux"])
+_sensitivity_E_3_digitized = np.genfromtxt("Sensitivity_E_3_7yr_PS_paper.txt", delimiter=", ", comments="#", names=["sinDec", "flux"])
+_discovery_potential_E_3_digitized = np.genfromtxt("DiscPot_E_3_7yr_PS_paper.txt", delimiter=", ", comments="#", names=["sinDec", "flux"])
+
 from .HPA_intermediat_plot_from_stefan_per_slack import bgd_extrapolation as _bgd_extrapolation
 
 N_spots_hist_threshold = {}
