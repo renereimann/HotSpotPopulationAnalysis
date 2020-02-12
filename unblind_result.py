cb15f334f75836c89f72cf1077af35b3f6992d4a
René Reimann <rene.reimann@rwth-aachen.de>
2020-02-12 12:34:51 +0100

    made all the changes to unblind_result.py

--- unblind_result.py
+++ unblind_result.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 
-from ps_analysis.hpa.utils import expectation
+from data_types import LocalWarmSpotExpectation
+from utils import HPA_analysis
 from skylab_data import SkylabAllSkyScan
 import cPickle
 import numpy as np
 
-file_name = "test_data/all_sky_scan_trial_iter2_skylab_sens_model_MCLLH3_season_IC_8yr_bestfit_spline_bin_mod_dec_2_spline_bin_mod_ener_2_prior_2.19_0.1_negTS_V2_inject_2.0_nside_256_followup_1_seed_unblind.pickle"
-scan = SkylabAllSkyScan(path=file_name)
+all_sky_scan_path = "test_data/all_sky_scan_trial_iter2_skylab_sens_model_MCLLH3_season_IC_8yr_bestfit_spline_bin_mod_dec_2_spline_bin_mod_ener_2_prior_2.19_0.1_negTS_V2_inject_2.0_nside_256_followup_1_seed_unblind.pickle"
+expectation_path = "test_data/from_poisson_test/HPA_nspot_expectation.pickle"
+bgd_trials_path = "test_data/HPA_TS_background_from_skylab.pickle"
+save_path = "test_data/unblinding_result.pickle"
+
+scan = SkylabAllSkyScan(path=all_sky_scan_path)
 scan.mask_hemisphere(dec_range=[np.radians(-3), np.radians(90)])
 spots = scan.get_local_warm_spots(log10p_threshold=2.0, min_ang_dist=1.0)
 
-expect = expectation("test_data/from_poisson_test/spline_expectation_cutoff_pVal_2.0_min_ang_dist_1.00.pickle")
-with open("/data/user/reimann/2017_10/HPA/bgd_trials/mc_trials/max_local_pVal_min_ang_dist_1.00_min_thres_2.00.pickle") as open_file:
-    hpa_trials_mc = cPickle.load(open_file)
-with open("/data/user/reimann/2017_10/HPA/bgd_trials/exp_trials/max_local_pVal_min_ang_dist_1.00_min_thres_2.00.pickle") as open_file:
-    hpa_trials_exp = cPickle.load(open_file)
+expect = LocalWarmSpotExpectation(expectation_path)
+analysis = HPA_analysis(expect)
+result = analysis.best_fit(spots["pVal"])
 
-# result
-HPA_TS, hpa_pthres, hpa_n_observed, hpa_n_expected = expect.poisson_test(spots["pVal"])
+with open(bgd_trials_path) as open_file:
+    bgd_trials = cPickle.load(open_file)
+hpa_ppost = np.sum(bgd_trials["hpa_ts"] > result["hpa_ts"], dtype=float) / len(bgd_trials)
 
-hpa_ppost_exp = np.sum(hpa_trials_exp > HPA_TS, dtype=float) / len(hpa_trials_exp)
-hpa_ppost_mc = np.sum(hpa_trials_mc > HPA_TS, dtype=float) / len(hpa_trials_mc)
-
-print HPA_TS, hpa_pthres, hpa_n_observed, hpa_n_expected, hpa_ppost_exp, hpa_ppost_mc
-
-result_dict = {"TS": HPA_TS, "p_thres": hpa_pthres, "#_expected": hpa_n_expected, "#_observed": hpa_n_observed, "pVal_post_exp": hpa_ppost_exp, "pVal_post_mc": hpa_ppost_mc, "spots": spots}
-with open("/home/reimann/software/python-modules/ps_analysis/hpa/unblinding_result.cPickle", "w") as open_file:
+result_dict = {"spots": spots, "best_fit_result": result, "pVal_post": hpa_ppost}
+with open(, "w") as open_file:
     cPickle.dump(result_dict, open_file)
+print result_dict
