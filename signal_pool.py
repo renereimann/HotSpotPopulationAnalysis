701eaac09eb341f78f50e1af36e735e6483b375c
René Reimann <rene.reimann@rwth-aachen.de>
2020-02-12 15:29:10 +0100

    removed trailing whitespaces

--- signal_pool.py
+++ signal_pool.py
@@ -3,13 +3,15 @@ try:
 except:
     pass
 
+from numpy.lib.recfunctions import append_fields
+
 class signal_pool(object):
     def __init__(self, **kwargs):
         self.set_seed( kwargs.pop("seed", None))
-        
+
     def set_seed(self, seed):
         self.random            = np.random.RandomState(seed)
-        
+
     def __str__(self):
         doc = ""
         print "Different declinations:", np.shape(self.injects)
@@ -20,14 +22,14 @@ class signal_pool(object):
         if hasattr(self, "flux_weights"):
             print self._weight_str
         return doc
-        
+
     def load_trials(self, pathes, pathes_add, pValue_calculator, pathes_add2=[]):
         """ Read in sensitivity trials from given file path
         You get back a list of injection trials, detector weights and declination positions.
-        You should previde two lists of file names. In addition you have to give a pValue_calculator, 
+        You should previde two lists of file names. In addition you have to give a pValue_calculator,
         thus the local pValue can be calculated."""
 
-        # we want to get uniform distributed trials in sindec, thus just take equidist trials in sinDec. 
+        # we want to get uniform distributed trials in sindec, thus just take equidist trials in sinDec.
         # This is how we generate the dag files
         equidist_sindec = 100
         equidist_dec = np.arcsin(np.linspace(np.sin(np.radians(-5)), 1, equidist_sindec+1))
@@ -37,13 +39,13 @@ class signal_pool(object):
             if np.min(np.abs(equidist_dec - float(dec_str))) < 1e-10: pathes_equidist.append(f)
         assert len(pathes_equidist) == equidist_sindec, "you did not get all declinations"
 
-        # det_weight should be used to correct for zenith acceptance 
+        # det_weight should be used to correct for zenith acceptance
         # incects will be a list of signal trials
         declinations, det_weight, injects, mus, fluxes = list(), list(), list(), list(), list()
-        for f1 in pathes_equidist: 
+        for f1 in pathes_equidist:
                 # read single source trials from sensitivity calculation
                 dec_key, mu, flux, inj = self.read_in_sens_file(f1)
-                
+
                 # add more files with same declination
                 float_re = "([+-]?\d*\.\d+|[+-]?\d+)"
                 dec_str = re.findall("_declination_{float_re}".format(**locals()), f1)[0]
@@ -52,26 +54,26 @@ class signal_pool(object):
                     dec_key_add, _, _,  inj_add = self.read_in_sens_file(f_add)
                     assert(np.fabs(dec_key   - dec_key_add)   < 2. * np.finfo(type(dec_key)).eps), "{dec_key}, {dec_key_add}".format(**locals())
                     inj = np.append(inj, inj_add)
-                
+
                 for f_add in pathes_add2:
                     if not dec_str in f_add: continue
                     inj_add = self.read_in_trial_file(f_add)
                     inj = np.append(inj, inj_add)
-                
+
                 # add pValue and relative weighting
                 inj = append_fields(inj, ["pVal", "w"],
                                  [np.ones(len(inj), dtype=np.float),
                                   np.ones(len(inj), dtype=np.float)])
                 inj["pVal"] = -np.log10(pValue_calculator(inj["TS"], dec_key))
-                # weights are 1/N for the moment, uniform distribution  
+                # weights are 1/N for the moment, uniform distribution
                 inj["w"]    = np.ones(len(inj), dtype=np.float) / len(inj)
                 inj         = inj[np.isfinite(inj["pVal"])]
-                
+
                 declinations.append(dec_key)
                 det_weight.append(mu/flux)
                 mus.append(mu)
                 fluxes.append(flux)
-                injects.append(inj) 
+                injects.append(inj)
 
         # the detector weights is normed relatively to the mean of the hemisphere
         det_weight        = np.asarray(det_weight)
@@ -85,9 +87,9 @@ class signal_pool(object):
         self.det_weight   = det_weight
         self.declinations = np.array(declinations)
         self.signal_pool  = np.concatenate(self.injects)
-    
+
     def read_in_sens_file(self, path):
-        """ Read in file and return declinationa flux2mu factor and trial-array with "n_inj", "TS" 
+        """ Read in file and return declinationa flux2mu factor and trial-array with "n_inj", "TS"
         Raises if file does not exist.
         """
         if not os.path.exists(path):
@@ -95,37 +97,37 @@ class signal_pool(object):
         with open(path) as open_file:
             job_args, trials = cPickle.load(open_file)
         dec_key    = trials.keys()[0]
-        mu         = np.mean(trials[dec_key][0]["mu"]) 
+        mu         = np.mean(trials[dec_key][0]["mu"])
         flux       = np.mean(trials[dec_key][0]["flux"])
-        
+
         return dec_key, mu, flux, trials[dec_key][1][["n_inj", "TS"]]
-        
+
     def read_in_trial_file(self, path):
-        """ Read in file and return declinationa flux2mu factor and trial-array with "n_inj", "TS" 
+        """ Read in file and return declinationa flux2mu factor and trial-array with "n_inj", "TS"
         Raises if file does not exist.
         """
         if not os.path.exists(path):
             raise ValueError("Could not load signal trials, because file does not exist.\nYou specified {path}".format(**locals()))
         with open(path) as open_file:
             job_args, trials = cPickle.load(open_file)
-        
+
         return trials[["n_inj", "TS"]]
-      
+
     def plot_sinDec_distribution(self):
         plt.figure()
         plt.hist(np.sin(self.declinations))
         plt.xlabel("$\sin(\delta)$")
         plt.ylabel("counts")
- 
+
     def plot_mu_2_flux(self):
         idxs = np.argsort(self.declinations)
         plt.plot(np.array(self.declinations)[idxs], np.array(self.flux)[idxs] / np.array(self.mu)[idxs])
         plt.xlabel("$\sin(\delta)$")
         plt.ylabel("flux/mu")
-    
+
     def _weight_str(self):
         return "N_inj: {}".format(self.n_inj)
-        
+
     def get_signal_weights(self, n_inj):
         """Calculates weights for each trials of injects. The mean expectation is n_inj.
         Take detector acceptance into account and poissoinan spread.
@@ -133,33 +135,33 @@ class signal_pool(object):
 
         # scale the number of injected events for all trials according to the relative weight
         # w = Poisson(lambda=1/N*det_w*n_inj_expectation).pdf(n_inj_simulated)
-        
+
         w = np.concatenate([poisson_weight(inject["n_inj"], n_inj * det_w) for inject, det_w in zip(self.injects, self.det_weight)])
-        
-        # sum over w has to be equal to len of injects because sum over w for each declination has to be 1. 
+
+        # sum over w has to be equal to len of injects because sum over w for each declination has to be 1.
         # the sum of weighted trials is 10% less then what it should be, missing edges
         # if not fullfilled essential regions of the poisson distributions are not covered
-        
+
         if np.fabs(1. - w.sum() / len(self.injects)) > 0.1:
             print("{}, {}".format(w.sum(), len(self.injects)))
             raise ValueError("Poissonian weighting not possible, too few trials.\nGenerate events within ns-range from {} to {}".format(np.min(n_inj*self.det_weight), np.max(n_inj*self.det_weight)) )
-        
+
         # draw equal distributed between sinDec
         w /= w.sum()
         assert np.all(w <= 0.1), "Too few trials in this regime, exit.\nSome events would have a probability larger then 10%.\nMax weights: {}".format( np.sort(w)[-10:] )
-        
+
         self.flux_weights   = w
         self.n_inj             = n_inj
-        
+
     def get_pseudo_experiment(self, nsrc):
         """ Draws n src p-values from signal pool and returns the local p-values as well as the n_injected summed over all nsrc."""
         if not hasattr(self, "random"):       raise ValueError("Random not yet set. Please set a seed with set_seed(seed)")
         if not hasattr(self, "flux_weights"): raise ValueError("Weights not yet initialized. Please initialize them with get_signal_weights(n_inj)")
-        
+
         sig       = self.random.choice(self.signal_pool, nsrc, p=self.flux_weights)
         n_tot_inj = sig["n_inj"].sum()
         return sig["pVal"], n_tot_inj
-       
+
     def save(self, save_path):
         state = {}
         state["flux_2_mu"]    = self.flux_2_mu
@@ -169,13 +171,13 @@ class signal_pool(object):
         state["mu"]           = self.mu
         with open(save_path, "w") as open_file:
             cPickle.dump(state, open_file, protocol=2)
-      
+
     def load(self, load_path, **kwargs):
         """ Loads signal pool from file,
         you can set in addition the seed"""
         if not os.path.exists(load_path): raise IOError("Inpath does not exist. {load_path}".format(**locals()))
         with open(load_path, "r") as open_file:
-            state = cPickle.load(open_file) 
+            state = cPickle.load(open_file)
         self.flux_2_mu    = state["flux_2_mu"]
         self.injects      = state["injects"]
         self.det_weight   = self.flux_2_mu/np.mean(self.flux_2_mu)
@@ -185,9 +187,9 @@ class signal_pool(object):
         self.signal_pool  = np.concatenate(self.injects)
         self.set_seed( kwargs.pop("seed", None))
         self.mean_flux_2_mu = np.mean(self.mu/self.flux)
-       
+
 class signal_pool_FIRESONG(signal_pool):
-        
+
     def load_firesong_representation(self, infile, density=None):
         fluxes = []
         with open(infile, "r") as open_file:
@@ -203,12 +205,12 @@ class signal_pool_FIRESONG(signal_pool):
         # as GeV is the basic unit this is the same as Phi0(at 1GeV) with change of units from 1/GeV cm^2 s to GeV/cm^2 s
         if density is not None:
             self.default_density = density
-    
+
     def get_pseudo_experiment(self, density=None):
         """ Draws poisson mus from firesong expectations and get corresponding p-values from signal pool and returns the local p-values as well as the n_injected summed over all sources."""
         # firesong mues are expectation values and have declination dependence already integrated
         # the final mu however is a poisson random number
-        
+
         if (density is not None) and (hasattr(self, "default_density")):
             if density > self.default_density:
                 raise ValueError("The given density is above the default density. That is not possible.")
@@ -229,17 +231,17 @@ class signal_pool_FIRESONG(signal_pool):
 
         n_tot_inj = sig["n_inj"].sum()
         return sig["pVal"], n_tot_inj
-                                 
+
 class signal_pool_SourceUnivers(signal_pool):
-        
+
     def load_SourceUnivers_representation(self, infile, seed=None):
         self.flux_dist = SourceCountDistribution(file=infile, seed=seed)
-        
+
     def get_pseudo_experiment(self, density):
         """ Draws poisson mus from firesong expectations and get corresponding p-values from signal pool and returns the local p-values as well as the n_injected summed over all sources."""
         # firesong mues are expectation values and have declination dependence already integrated
         # the final mu however is a poisson random number
-        
+
         dec_idx = []
         mus = []
         n_sources = self.flux_dist.calc_N(density)
