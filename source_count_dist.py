a071c9fc63a19eaae219863e1138ba61bfae5cbe
René Reimann <rene.reimann@rwth-aachen.de>
2020-02-13 14:43:20 +0100

    added __str__ functions

--- source_count_dist.py
+++ source_count_dist.py
@@ -2,19 +2,26 @@ class SourceCountDistribution(object):
     def __init__(self):
         pass
 
+    def __str__(self):
+        return NotImplemented
+
     def get_fluxes(self, **kwargs):
         return NotImplemented
 
 class SourceCountDistEqualFluxAtEarth(SourceCountDistribution):
     def __init__(self, **kwargs):
-        self.standard_candle_flux = kwargs.pop("phi_inj", None)
+        self.source_flux = kwargs.pop("phi_inj", None)
         self.n_sources = kwargs.pop("n_sources", None)
 
+    def __str__(self):
+        return "phi_inj_%s_nsrc_%s"%(self.source_flux, self.n_source)
+
     def get_fluxes(self, **kwargs):
-        return self.standard_candle_flux*np.ones(self.n_sources)
+        return self.source_flux*np.ones(self.n_sources)
 
 class SourceCountDistFIRESONG(SourceCountDistribution):
     def load_firesong_representation(self, infile, density=None):
+        self.infile = infile
         fluxes = []
         with open(infile, "r") as open_file:
             for line in open_file:
@@ -27,6 +34,9 @@ class SourceCountDistFIRESONG(SourceCountDistribution):
         # as GeV is the basic unit this is the same as Phi0(at 1GeV) with change of units from 1/GeV cm^2 s to GeV/cm^2 s
         if density is not None:
             self.default_density = density
+
+    def __str__(self):
+        return "infile_%s_density_%s"%(self.infile, self.density)
 
     def get_fluxes(self, density=None):
         r"""Draws fluxes from a firesong generated source count distribution.
