dea4fb683e1e58223597a79a5dab51b794e83f4b
René Reimann <rene.reimann@rwth-aachen.de>
2020-02-13 12:03:50 +0100

    switched to new implementation of classes, changed the data pathes to use test_data

--- prepare_signal_pool.py
+++ prepare_signal_pool.py
@@ -2,16 +2,15 @@
 
 import glob, os
 from SingleSpotTS2pValueParametrization import SingleSpotTS2pValueParametrization
-from ps_analysis.hpa.utils import signal_pool
+from utils import SingleSpotTrialPool
 
-indir_sens  = "/data/user/reimann/2017_10/sensitivity/mc_trials_fixed_negTS/E-2.0/sindec_bandwidth_1deg/"
-indir_sens2 = "/data/user/reimann/2017_10/sensitivity/mc_trials_fixed_negTS/E-2.0/sindec_bandwidth_1deg_TSVal/"
+indir_sens  = "test_data/single_spot_trials/"
+parametrization_path = "test_data/parametrization_TS_to_p_value_exponential_fit_above_threshold_5_from_mc.param"
 
-sens_files      = sorted(glob.glob(os.path.join(indir_sens,  "sens_*.pickle")))
-additional_file = sorted(glob.glob(os.path.join(indir_sens2, "sens_*.pickle")))
+sens_files = sorted(glob.glob(os.path.join(indir_sens,  "sens_*.pickle")))
 
-pVal = SingleSpotTS2pValueParametrization(path="test_data/parametrization_TS_to_p_value_exponential_fit_above_threshold_5_from_mc.param")
-sig_pool = signal_pool(seed=None)
+pVal = SingleSpotTS2pValueParametrization(path=parametrization_path)
+
+sig_pool = SingleSpotTrialPool(seed=None)
 sig_pool.load_trials(sens_files, pValue_calculator=pVal)
-
-sig_pool.save("/data/user/reimann/2017_10/HPA/signal_pool.cPickle")
+sig_pool.save("test_data/signal_pool.cPickle")
