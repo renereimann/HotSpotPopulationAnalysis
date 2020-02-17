8bd5809a340150f6aa65a4a0d826183f4f5bf3b4
René Reimann <rene.reimann@rwth-aachen.de>
2020-02-17 17:03:26 +0100

    removed unused data types

--- data_types.py
+++ data_types.py
@@ -96,11 +96,3 @@ class LocalWarmSpotExpectation(object):
             raise RuntimeError("No parametrization loaded or generated.")
         # we made the spline in log10
         return 10**self._log10_n_expected(logP_thres)
-
-class HPA_TS_Parametrization(object):
-    def __init__(self, **kwargs):
-        pass
-
-class Sensitivity(object):
-    def __init__(self, **kwargs):
-        pass
