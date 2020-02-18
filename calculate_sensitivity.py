26e8578afc86168345b5d23e7a939af96eafebe8
René Reimann <rene.reimann@rwth-aachen.de>
2020-02-18 15:45:39 +0100

    heavy clean up of the script, added option to the senesitivity_result class, added argparser, removed unneeded functions and variables, fixed minor things so that the script is now running, added some nice printing, defined quantities to calculate

--- calculate_sensitivity.py
+++ calculate_sensitivity.py
@@ -5,30 +5,45 @@ import cPickle as pickle
 import numpy as np
 from scipy.optimize import minimize
 from scipy.stats import gamma, kstest
-from statistics import llh2Sigma, sigma2pval
+from statistics import llh2Sigma, sigma2pval, poisson_percentile
 from utils import dec_range
 
 class sensitivity_result(object):
-    def __init__(self, CL=None, alpha=None, beta=None, TS_thres=None, disc_pot_like=True, label=None, source_model=None):
+    def __init__(self, TS_thres=None, alpha=None, beta=None, label=None):
         # define kind of CL level
+        self.TS_thres = TS_thres
         self.alpha = alpha
         self.beta = beta
-        self.TS_thres = TS_thres
-        self.CL = CL
-        self.disc_pot_like = disc_pot_like
-        self.limit_like = not disc_pot_like
         self.label = label
 
         # tot injected at CL level
-        self.mu = None
+        self.tot_n_inj = None
+        self.success = False
 
-        # define mu -> flux conversion factor
+        # define tot_n_inj -> flux conversion factor
         self.flux_per_mu = None
         self.flux_units = "1/GeV cm^2 s"
         self.flux_pivot_point = "1 GeV"
 
         # source mode specific
-        self.source_model = source_model
+        self.source_model = None
+
+    def __str__(self):
+        return "Sensitivity Result: "+str(self.__dict__)
+
+    @property
+    def disc_pot_like(self):
+        return self.beta == 0.5
+
+    @property
+    def limit_like(self):
+        return not self.disc_pot_like
+
+    @property
+    def CL(self):
+        if self.disc_pot_like:
+            return self.alpha
+        return self.beta
 
 def make_gamma_fit(trials, scan_likelihood=[16,16], verbose=False):
     r"""Fits a Gamma Distribution to the HPA-TS values. The Goodness of fit
@@ -61,17 +76,11 @@ def make_gamma_fit(trials, scan_likelihood=[16,16], verbose=False):
     median_lower = trials[int(len(trials)/2.-np.sqrt(len(trials))*0.5)]
     median_upper = trials[int(len(trials)/2.+np.sqrt(len(trials))*0.5)+1]
 
-    TS_vs_quantile = lambda quantile: gamma(*params).isf(quantile)
-    p_3sigma = sigma2pval(3, oneSided=True)
-    p_5sigma = sigma2pval(5, oneSided=True)
-    ext_3sig = gamma(*params).isf(p_5sigma)
-    ext_5sig = gamma(*params).isf(p_5sigma)
-
     result = {}
     result["params"] = params
     result["ks_pval"] = ks_gamma
     result["median"] = (median, (median_lower, median_upper))
-    result["TS_vs_quantile"] = TS_vs_quantile
+    result["TS_vs_quantile"] = lambda quantile: gamma(*params).isf(quantile)
 
     if verbose:
         print "Fit parameters:", params
@@ -120,14 +129,14 @@ def get_mu_2_flux_factor(dec_range=dec_range, spectral_index=2.):
         mu -> flux conversion factor
     """
 
-    version_path = "/data/ana/analyses/northern_tracks/version-002-p06/"
+    version_path = "/home/rene/Desktop/version-002-p06/"
     season_list = [("dataset_8yr_fit_IC59_MC_compressed.npy",        "GRL/dataset_8yr_fit_IC59_exp_compressed.npy"),
                   ("dataset_8yr_fit_IC79_MC_compressed.npy",         "GRL/dataset_8yr_fit_IC79_exp_compressed.npy"),
                   ("dataset_8yr_fit_IC86_2011_MC_compressed.npy",    "GRL/dataset_8yr_fit_IC86_2011_exp_compressed.npy"),
                   ("dataset_8yr_fit_IC86_2012_16_MC_compressed.npy", "GRL/dataset_8yr_fit_IC86_2012_16_exp_compressed.npy"),]
 
     denominator = 0
-    for mc_path, grl_path in enumerate(season_list):
+    for mc_path, grl_path in season_list:
         mc = np.load(os.path.join(version_path, mc_path))
         grl = np.load(os.path.join(version_path, grl_path))
         livetime = np.sum(grl["livetime"])
@@ -138,25 +147,6 @@ def get_mu_2_flux_factor(dec_range=dec_range, spectral_index=2.):
 
     solAng = 2. * np.pi * (np.sin(max(dec_range)) - np.sin(min(dec_range)))
     return solAng/denominator
-
-def get_files_for_nsrc(nsrc, glob_path):
-    files = sorted(glob.glob(glob_path))
-
-    # first get the nsrc number of each file
-    nsrc_file_list = []
-    for f in files:
-        # get FIRST match in a string
-        number = re.search('_'+8*'[0-9]', f)
-        number = int(number.group(0)[1:])
-        nsrc_file_list.append((number, f))
-
-    # select just file pathes with nsrc in path
-    selected_file_list = []
-    for f in nsrc_file_list:
-        if f[0] == nsrc:
-            selected_file_list.append(f[1])
-
-    return np.concatenate([np.load(f) for f in selected_file_list])
 
 def sens_estimation(trials, TS_thres, perc, eps=2.5):
     """ Estimates sensitivity and discovery potential for a fixed number of nsources.
@@ -182,7 +172,6 @@ def sens_estimation(trials, TS_thres, perc, eps=2.5):
     # function that has a deep minimum at intersection of TS_quantile and perc
     fun = lambda n: np.log10((TS_quantile(n)- perc)**2)
 
-
     bounds = [np.percentile(trials["n_inj"], [eps, 100.-eps])]
 
     # Brute Force for seed
@@ -190,7 +179,7 @@ def sens_estimation(trials, TS_thres, perc, eps=2.5):
     seed = mu[np.argmin([fun(i) for i in mu])]
 
     # minimize function
-    res = minimize(fun, x0=[seed], method="L-BFGS-B", bounds=bounds, approx_grad=True)
+    res = minimize(fun, x0=[seed], method="L-BFGS-B", bounds=bounds)#, approx_grad=True)
 
     # calculate uncertainties on percentile
     beta, beta_err = poisson_percentile(res["x"], trials["n_inj"], trials["logP"], TS_thres)
@@ -209,55 +198,80 @@ def sens_estimation(trials, TS_thres, perc, eps=2.5):
 parser = argparse.ArgumentParser()
 parser.add_argument("--backgroung_HPA_trials",
                     type=str,
-                    required=True,
+                    #default="test_data/HPA_TS_background_from_skylab.pickle",
+                    default="test_data/max_local_pVal_min_ang_dist_1.00_min_thres_2.00.pickle",
                     help="Give inpath.")
-args = parser.parse_args()
+parser.add_argument("--outfile",
+                    type=str,
+                    default="test_data/sensitivity_test_nsrc_256.pickle",
+                    help="Give inpath.")
+parser.add_argument("--signal_files",
+                    type=str,
+                    nargs='+',
+                    default=["test_data/HPA_signal_trials_nsrc_00000256_nsrcIdx_00000000.npy", "test_data/HPA_signal_trials_nsrc_00000256_nsrcIdx_00000001.npy",
+                             "test_data/HPA_signal_trials_nsrc_00000256_nsrcIdx_00000002.npy", "test_data/HPA_signal_trials_nsrc_00000256_nsrcIdx_00000003.npy",
+                             "test_data/HPA_signal_trials_nsrc_00000256_nsrcIdx_00000004.npy", "test_data/HPA_signal_trials_nsrc_00000256_nsrcIdx_00000005.npy",
+                             "test_data/HPA_signal_trials_nsrc_00000256_nsrcIdx_00000006.npy", "test_data/HPA_signal_trials_nsrc_00000256_nsrcIdx_00000007.npy",
+                             "test_data/HPA_signal_trials_nsrc_00000256_nsrcIdx_00000008.npy", "test_data/HPA_signal_trials_nsrc_00000256_nsrcIdx_00000009.npy",],
+                    help="List of input files. Input files should contain a list of extracted background populations.")
+parser.add_argument("--unblinded_value",
+                    type=float,
+                    default=None, # 1.379
+                    help="If you have unblinded give the TS value here and upper limits are calculated.")
+args, source_model_args = parser.parse_known_args()
 
 print "Run", os.path.realpath(__file__)
 print "Use arguments:", args
 print
 
-# Settings
-TS_val_keys        = ["median", "3sig", "5sig", "UL"]
-beta               = [0.9,      0.5,    0.5,    0.9]                    # quantiles of the signal TS distribution that should beat the correspont background values
-alpha              = [0.5,      "3sig", "5sig", unblind_value]
-sig_trial_path     = "/data/user/reimann/2017_10/HPA/sig_trials/mc_no_astro_bgd_2_19/HPA_signal_trials_nsrc_0000*_00*.npy"
-nsrc_list          = np.array([1,2,4,8,16,32,64,128,256,512,1024,2048]) # Number of Sources for which to calculate sensitivities
-nsrc
-unblind_value      = 1.37962118438
-
-
 # get background HPA trials
 with open(args.backgroung_HPA_trials, "r") as open_file:
     backgroung_HPA_trials = pickle.load(open_file)
 # fit the background distribution and extrapolate
+#gamma_fit = make_gamma_fit(backgroung_HPA_trials["hpa_ts"])
 gamma_fit = make_gamma_fit(backgroung_HPA_trials)
 
 # Get mu -> flux factor
 mu2flux = get_mu_2_flux_factor()
-print "Mu to flux conversion factor:", mu2flux
+print "Flux per mu in 1/GeV cm^2 s @ 1 GeV:", mu2flux
 
-# get data
-sig_trials = get_files_for_nsrc(nsrc, sig_trial_path)
+# Settings
+sig_trials = np.concatenate([np.load(f) for f in args.signal_files])
+source_model = source_model_args
+print "Source Model contains", source_model
+print
 
+# define the quantities to calculate
+results = [sensitivity_result(alpha=0.5,                          beta=0.9, label="sens"),
+           sensitivity_result(alpha=sigma2pval(3, oneSided=True), beta=0.5, label="disc pot 3sig"),
+           sensitivity_result(alpha=sigma2pval(5, oneSided=True), beta=0.5, label="disc pot 5sig")]
+if args.unblinded_value is not None:
+    results.append(sensitivity_result(TS_thres=args.unblinded_value, beta=0.9, label="UL"))
 
-results = []
 # loop over sensitivity and discovery potential TS
-bgd_TS_vals =[gamma_fit[k][0] if k!="UL" else unblind_value for k in TS_val_keys]
-
-def __init__(self, CL=None, alpha=None, beta=None, TS_thres=None, disc_pot_like=True, label=None):
-res = sensitivity_result(TS_thres=bgd_TS_vals, beta=, flux_per_mu=mu2flux, source_model={"nsrc": nsrc})
-
 for res in results:
+    res.source_model = source_model
+    res.flux_per_mu = mu2flux
+    # if TS-threshold is not given, we get it from the background distribution
+    if res.TS_thres is None:
+        if res.alpha == 0.5:
+            res.TS_thres = gamma_fit["median"][0]
+        else:
+            res.TS_thres = gamma_fit["TS_vs_quantile"](res.alpha)
     try:
         # SENSITIVITY estimation
-        mu, w, b, b_err = sens_estimation(sig_trials, res.TS_thres, res.beta)
-        res.mu = mu
+        tot_n_inj, w, b, b_err = sens_estimation(sig_trials, res.TS_thres, res.beta)
+        # needed for plotting
+        # nsrc sig_trials, w, b, b_err
+        res.tot_n_inj = tot_n_inj
         res.success = True
-    except:
+    except Exception as e:
+        print e
         continue
 
-    # needed for plotting
-    # nsrc sig_trials, val_i, beta_i, eps, mu, w, b, b_err
+print
+for res in results:
+    print res
 
-save_path = "test_data/hpa_sensitivity_and_limit_bgd_wo_astro_2_19.pickle"
+with open(args.outfile, "w") as open_file:
+    pickle.dump(results, open_file, protocol=2)
